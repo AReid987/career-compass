f3a28291fba06871da271218a7af3a3ce0896836
Antonio Reid <read.musik@gmail.com>
2024-02-18 20:19:42 -0600

    create apps

---
+++ apps/client/src/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import React from "react";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Career Compass",
+  description: "Navigating your job search with AI",
+  authors: [{ name: "Antonio Reid" }],
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
