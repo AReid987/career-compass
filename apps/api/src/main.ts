d64e432c4d45c96e1832c449b7343b38ea24fe3a
Antonio Reid <read.musik@gmail.com>
2024-02-19 23:27:24 -0600

    add validate script

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,10 +1,12 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { FlubErrorHandler } from 'nestjs-flub';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { FlubErrorHandler } from 'nestjs-flub'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalFilters(new FlubErrorHandler());
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    snapshot: true,
+  })
+  app.useGlobalFilters(new FlubErrorHandler())
+  await app.listen(3000)
 }
-bootstrap();
+bootstrap()
--- apps/client/src/app/layout.tsx
+++ apps/client/src/app/layout.tsx
@@ -1,24 +1,24 @@
-import type { Metadata } from "next";
-import React from "react";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next'
+import React from 'react'
+import { Inter } from 'next/font/google'
+import './globals.css'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Career Compass",
-  description: "Navigating your job search with AI",
-  authors: [{ name: "Antonio Reid" }],
-};
+  title: 'Career Compass',
+  description: 'Navigating your job search with AI',
+  authors: [{ name: 'Antonio Reid' }],
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  );
+  )
 }
