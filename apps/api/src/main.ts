3fef5127c7980a9cb925ddb5b60e89147aa68fa1
Antonio Reid <read.musik@gmail.com>
2024-02-19 20:11:31 -0600

    add flub

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { FlubErrorHandler } from 'nestjs-flub';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new FlubErrorHandler());
   await app.listen(3000);
 }
 bootstrap();
