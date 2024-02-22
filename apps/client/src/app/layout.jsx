6e45b3292b13c72082dc3611f39427e782ec46b6
Antonio Reid <read.musik@gmail.com>
2024-02-21 23:51:24 -0600

    wip megalinter , autofix CI actions

--- apps/client/src/app/layout.jsx
+++ apps/client/src/app/layout.jsx
@@ -1,21 +1,14 @@
-import type { Metadata } from 'next'
 import React from 'react'
 import { Inter } from 'next/font/google'
 import './globals.css'
-
-const inter = Inter({ subsets: ['latin'] })
-
-export const metadata: Metadata = {
+var inter = Inter({ subsets: ['latin'] })
+export var metadata = {
   title: 'Career Compass',
   description: 'Navigating your job search with AI',
   authors: [{ name: 'Antonio Reid' }]
 }
-
-export default function RootLayout({
-  children
-}: Readonly<{
-  children: React.ReactNode
-}>) {
+export default function RootLayout(_a) {
+  var children = _a.children
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
