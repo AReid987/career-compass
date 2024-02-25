4f13d5ec003d7a90adf07bf54aa20fd1af6830fd
Antonio Reid <read.musik@gmail.com>
2024-02-24 19:10:46 -0600

    ci fix

--- apps/api/src/app.controller.spec.ts
+++ apps/api/src/app.controller.spec.ts
@@ -4,18 +4,19 @@ import { AppService } from './app.service'
 
 describe('AppController', () => {
   let appController: AppController
+  let app: TestingModule
+
+  afterAll(async () => {
+    await app.close()
+  })
 
   beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
+    app = await Test.createTestingModule({
       controllers: [AppController],
       providers: [AppService]
     }).compile()
 
     appController = app.get<AppController>(AppController)
-
-    afterAll(async () => {
-      await app.close()
-    })
   })
 
   describe('root', () => {
